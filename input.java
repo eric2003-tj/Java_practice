7f0ea9ba72a0dff133658291543a117e40465542
Eric <102838561+eric2003-tj@users.noreply.github.com>
2023-01-12 20:14:56 +0800

    Create input.java

---
+++ input.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Introduction {
+  public static void main( String[] args ) {
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter your name");
+    String userName = input.next();
+    System.out.printf("Hello %s! It's nice to meet you.",userName);
+  }
+}
