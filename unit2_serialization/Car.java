d142adbe65b25062f93ca854c4ff38872dc9a42b
Eric <102838561+eric2003-tj@users.noreply.github.com>
2023-01-14 10:36:47 +0800

    implementing read_object and write_object method

--- unit2_serialization/Car.java
+++ unit2_serialization/Car.java
@@ -10,14 +10,32 @@ public class Car implements Serializable {
   private String make;
   private int year;
   private static final long serialVersionUID = 1L;
+  private Engine engine;
 
   public Car(String make, int year) {
     this.make = make;
     this.year = year;
+    this.engine = new Engine(2.4, 6);
   }
 
+ private void writeObject(ObjectOutputStream stream) throws IOException {
+    stream.writeObject(this.make);
+    stream.writeInt(this.year);
+    stream.writeDouble(this.engine.getLiters());
+    stream.writeInt(this.engine.getCylinders());
+  }
+ 
+  private void readObject(ObjectInputStream stream) throws IOException, ClassNotFoundException {
+   this.make = (String) stream.readObject();
+   this.year = (int) stream.readInt();
+   double liters = (double) stream.readDouble();
+   int cylinders = (int) stream.readInt();
+   this.engine = new Engine(liters,cylinders);
+  }    
+
+
   public String toString(){
-    return String.format("Car make is: %s, Car year is: %d", this.make, this.year);
+    return String.format("Car make is: %s, Car year is: %d, %s", this.make, this.year, this.engine);
   }
 
   public static void main(String[] args) throws FileNotFoundException, IOException, ClassNotFoundException {
@@ -27,13 +45,16 @@ public class Car implements Serializable {
     ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
     objectOutputStream.writeObject(toyota);
     objectOutputStream.writeObject(honda);
+
     FileInputStream fileInputStream = new FileInputStream("cars.txt");
     ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
-    Car toyotaCopy = (Car)objectInputStream.readObject();
-    Car hondaCopy = (Car)objectInputStream.readObject();
+
+    Car toyotaCopy = (Car) objectInputStream.readObject();
+    Car hondaCopy = (Car) objectInputStream.readObject();
+
     boolean isSameObject = toyotaCopy == toyota;
-    System.out.println(toyotaCopy);
-    System.out.println(toyota);
-    System.out.println(isSameObject);
+    System.out.println("Toyota (Copy) - "+toyotaCopy);
+    System.out.println("Toyota (Original) - "+toyota);
+    System.out.println("Is same object: "+ isSameObject);
   }
 }
